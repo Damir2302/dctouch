982957247ed889877b51287e13ec234c83f8bae5
Damir S <damir23.02@mail.ru>
2023-07-27 00:43:30 +0300

    fixed

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,25 +1,17 @@
 $(document).ready(function() {
 
-    let anchors = document.querySelectorAll('.js-anchor')
-    if (anchors.length > 0) {
-        anchors.forEach(anchor => {
-            anchor.onclick = function (e) {
-                e.preventDefault();
-                document.querySelector(`#${anchor.href.split('#')[1]}`).scrollIntoView({
-                    block: 'nearest',
-                    behavior: 'smooth',
-                })
-            }
-        })
-    }
-
-    document.getElementById('jump-to-menu').addEventListener("click", function() {
-        window.scroll({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        })
-    })
+    // let anchors = document.querySelectorAll('.js-anchor')
+    // if (anchors.length > 0) {
+    //     anchors.forEach(anchor => {
+    //         anchor.onclick = function (e) {
+    //             e.preventDefault();
+    //             document.querySelector(`#${anchor.href.split('#')[1]}`).scrollIntoView({
+    //                 block: 'nearest',
+    //                 behavior: 'smooth',
+    //             })
+    //         }
+    //     })
+    // }
 
     $('.play-icon').on('click', function() {
         $(this).addClass('hidden')
@@ -41,12 +33,42 @@ $(document).ready(function() {
         $(this).closest('#menu').toggleClass('active')
     })
 
-    if ($(window).width() >= 1200) {
-        const scroll = new LocomotiveScroll({
+    var scroll
+
+    function initScroll() {
+        scroll = new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true,
             direction: 'horizontal',
-        })
+        });
     }
+
+    function destroyScroll() {
+        if (scroll) {
+            scroll.destroy();
+            scroll = null;
+        }
+    }
+
+    function toggleScroll() {
+        const screenWidth = $(window).width();
+
+        if (screenWidth >= 1200) {
+            initScroll();
+        } else {
+            destroyScroll();
+        }
+    }
+
+    // Инициализация плагина при загрузке страницы
+    toggleScroll()
+
+    // Включение или отключение плагина при изменении размера экрана
+    window.addEventListener('resize', toggleScroll);
+
+    $('.js-anchor').on('click', function(e) {
+        e.preventDefault()
+        scroll.scrollTo(document.querySelector('#form'))
+    })
 
 })
